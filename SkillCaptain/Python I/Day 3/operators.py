005a1c657852c8cf641ec4ae3f4790f2a371b1fb
Gary <garysiu@outlook.com>
2023-10-24 07:30:52 +0100

    Submit SkillCaptain Python I, days 1-3 exercises

---
+++ SkillCaptain/Python I/Day 3/operators.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+add = num1 + num2
+print(add)
+
+sub = num1 - num2
+print(sub)
+
+product = num1 * num2
+print(product)
+
+quotient = num1 / num2
+print(quotient)
