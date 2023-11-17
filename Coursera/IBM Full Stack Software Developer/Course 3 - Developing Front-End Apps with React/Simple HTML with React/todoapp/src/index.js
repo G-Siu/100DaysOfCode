8bb488ccaf48e2b40f36cfe655f780ee44874280
Gary <garysiu@outlook.com>
2023-11-17 15:03:26 +0000

    Delete create-react-app files due to large size

--- Coursera/IBM Full Stack Software Developer/Course 3 - Developing Front-End Apps with React/Simple HTML with React/todoapp/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import App from './App';
-import { createRoot } from 'react-dom/client';
-
-// This is how it it is done from React 18 onwards
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App color="blue" size="20px"/>);
