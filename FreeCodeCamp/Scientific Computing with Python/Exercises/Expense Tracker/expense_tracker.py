946f36df3e622d3c16fab11997a6861032719b5f
Gary <garysiu@outlook.com>
2023-12-22 09:56:20 +0000

    Submit expense tracker

--- FreeCodeCamp/Scientific Computing with Python/Exercises/Expense Tracker/expense_tracker.py
+++ FreeCodeCamp/Scientific Computing with Python/Exercises/Expense Tracker/expense_tracker.py
@@ -1,7 +1,7 @@
 # Learn Lambda Functions by Building an Expense Tracker
 # Name: G Siu
 # Date created: 21st December 2023
-# Date modified: 21st December 2023
+# Date modified: 22nd December 2023
 
 def add_expense(expenses, amount, category):
     expenses.append({'amount': amount, 'category': category})
@@ -17,4 +17,44 @@ def total_expenses(expenses):
 
 
 def filter_expenses_by_category(expenses, category):
-    lambda expense: expense['category'] == category
+    return filter(lambda expense: expense['category'] == category, expenses)
+
+
+def main():
+    expenses = []
+    while True:
+        print('\nExpense Tracker')
+        print('1. Add an expense')
+        print('2. List all expenses')
+        print('3. Show total expenses')
+        print('4. Filter expenses by category')
+        print('5. Exit')
+
+        choice = input('Enter your choice: ')
+
+        if choice == '1':
+            amount = float(input('Enter amount: '))
+            category = input('Enter category: ')
+            add_expense(expenses, amount, category)
+
+        elif choice == '2':
+            print('\nAll Expenses:')
+            print_expenses(expenses)
+
+        elif choice == '3':
+            print('\nTotal Expenses: ', total_expenses(expenses))
+
+        elif choice == '4':
+            category = input('Enter category to filter: ')
+            print(f'\nExpenses for {category}:')
+            expenses_from_category = filter_expenses_by_category(expenses,
+                                                                 category)
+            print_expenses(expenses_from_category)
+
+        elif choice == '5':
+            print('Exiting the program.')
+            break
+
+
+if __name__ == "__main__":
+    main()
