8b9a17dde04202be20bb6ada32868f3e46a9a9fc
Gary <garysiu@outlook.com>
2023-12-23 13:48:49 +0000

    Submit password generator exercise

---
+++ FreeCodeCamp/Scientific Computing with Python/Exercises/Password Generator/password_generator.py
@@ -0,0 +1,46 @@
+# Learn Regular Expressions by Building a Password Generator
+# Name: G Siu
+# Date created: 23rd December 2023
+# Date modified: 23rd December 2023
+
+import re
+import secrets
+import string
+
+
+def generate_password(length=16, nums=1, special_chars=1, uppercase=1,
+                      lowercase=1):
+    # Define the possible characters for the password
+    letters = string.ascii_letters
+    digits = string.digits
+    symbols = string.punctuation
+
+    # Combine all characters
+    all_characters = letters + digits + symbols
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
+
+        constraints = [
+            (nums, r'\d'),
+            (special_chars, fr'[{symbols}]'),
+            (uppercase, r'[A-Z]'),
+            (lowercase, r'[a-z]')
+        ]
+
+        # Check constraints
+        if all(
+                constraint <= len(re.findall(pattern, password))
+                for constraint, pattern in constraints
+        ):
+            break
+
+    return password
+
+
+if __name__ == "__main__":
+    new_password = generate_password()
+    print('Generated password:', new_password)
