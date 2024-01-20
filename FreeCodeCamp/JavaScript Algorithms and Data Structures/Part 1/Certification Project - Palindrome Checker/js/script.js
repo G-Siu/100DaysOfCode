bc1c21798e7f237bee6ac000230b6339486bae90
Gary <garysiu@outlook.com>
2024-01-20 07:04:46 +0000

    Submit palindrome partially functioning

--- FreeCodeCamp/JavaScript Algorithms and Data Structures/Part 1/Certification Project - Palindrome Checker/js/script.js
+++ FreeCodeCamp/JavaScript Algorithms and Data Structures/Part 1/Certification Project - Palindrome Checker/js/script.js
@@ -3,7 +3,17 @@ function cleanInputString(str) {
     return str.replace(regex, "");
 }
 
-userInput = document.getElementById("text-input");
-console.log(userInput);
-// const inputResult = document.getElementByClassName('input-result');
-// inputResult.innerText = `${}`
+const checkBtn = document.getElementById("check-btn");
+
+checkBtn.addEventListener("click", () => {
+    const textInput = document.getElementById("text-input").value;
+    const cleanInput = cleanInputString(textInput).toLowerCase();
+    const reverseInput = cleanInput.split("").reverse().join("");
+    const inputResult = document.getElementById("result");
+    if (cleanInput === reverseInput) {
+        inputResult.innerHTML = `<p><strong>${textInput}</strong> is a palindrome.</p>`;
+    } else {
+        inputResult.innerHTML = `<p><strong>${textInput}</strong> is not a palindrome.</p>`;
+    }
+});
+
