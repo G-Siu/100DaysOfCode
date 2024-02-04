132b6c1542efc6425252c8e3b7bcb5f5359be1ad
Gary <garysiu@outlook.com>
2024-02-04 09:49:12 +0000

    Submit first two JSX exercises, first time achieving JSX render

---
+++ FreeCodeCamp/Front End Development Libraries/React/JSX/Complex/script.js
@@ -0,0 +1,15 @@
+const JSX = (
+    <div>
+        <h1></h1>
+        <p></p>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+);
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(JSX);
