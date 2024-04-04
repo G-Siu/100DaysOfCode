a112b19fb90e66947ef49794e5c97dbc9b0ffe92
Gary <garysiu@outlook.com>
2024-04-04 22:53:58 +0100

    Submit activity 2 basics

---
+++ Edinburgh College/JS/activity2.js
@@ -0,0 +1,3 @@
+let firstNumber = Number(prompt("First number: ", "4"));
+let secondNumber = Number(prompt("Second number: ", "2"));
+console.log(`Division of ${firstNumber} and ${secondNumber} is ${firstNumber / secondNumber}`);
