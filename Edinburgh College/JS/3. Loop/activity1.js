1bddbd46f2bd66580f5234509e14a9c66269920b
Gary <garysiu@outlook.com>
2024-04-05 18:04:08 +0100

    Submit loop exercises

---
+++ Edinburgh College/JS/3. Loop/activity1.js
@@ -0,0 +1,9 @@
+// Loop from 0 to 15
+for (let i = 0; i <= 15; i++) {
+    // Check if remainder is 0
+    if (i % 2 == 0) {
+        console.log(`${i} is even`)
+    } else {
+        console.log(`${i} is odd`)
+    }
+}
---
+++ Edinburgh College/JS/3. Loop/activity2.js
@@ -0,0 +1,5 @@
+number = parseInt(prompt("Input a number: "))   // Get a number from input
+// Iterate from 1 to 10
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`)
+}
