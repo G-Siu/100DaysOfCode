009b3f12639a59e8a62d8f9f5cd21ede4527e207
Gary <garysiu@outlook.com>
2024-05-06 21:33:36 +0100

    Submit SQL and start of Python

---
+++ Edinburgh College/Python/1. Python Basics/activity2.py
@@ -0,0 +1,8 @@
+# Get first number
+first_number = int(input("Enter first number: "))
+# Get second number
+second_number = int(input("Enter second number: "))
+
+# Display divided answer
+print("%d / %d = %.2f" % (first_number, second_number, first_number /
+                          second_number))
